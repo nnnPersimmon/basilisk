92e9746bf68e1d15e1a00911fdb112958a9acb04
n-benchoubane <n.benchoubane@motsai.com>
2024-07-20 18:59:19 -0400

    fix code

--- scenarios/scenarioSensorThermal.py
+++ scenarios/scenarioSensorThermal.py
@@ -5,20 +5,10 @@ import numpy as np
 from Basilisk import __path__
 from Basilisk.architecture import messaging
 from Basilisk.fswAlgorithms import locationPointing, mrpFeedback
-from Basilisk.simulation import (
-    ephemerisConverter,
-    extForceTorque,
-    sensorThermal,
-    simpleNav,
-    spacecraft,
-)
-from Basilisk.utilities import (
-    SimulationBaseClass,
-    macros,
-    orbitalMotion,
-    simIncludeGravBody,
-    unitTestSupport,
-)
+from Basilisk.simulation import (ephemerisConverter, extForceTorque,
+                                 sensorThermal, simpleNav, spacecraft)
+from Basilisk.utilities import (SimulationBaseClass, macros, orbitalMotion,
+                                simIncludeGravBody, unitTestSupport)
 
 bskPath = __path__[0]
 fileName = os.path.basename(os.path.splitext(__file__)[0])
@@ -28,37 +18,61 @@ simTaskName = "simTask"
 simProcessName = "simProcess"
 
 
-def run(show_plots):
-    """
-    The scenarios can be run with the followings setups parameters:
+intertia = [900.0, 0.0, 0.0, 0.0, 800.0, 0.0, 0.0, 0.0, 600.0]
 
-    Args:
-        show_plots (bool): Determines if the script should display plots
 
-    """
-
-    scSim = SimulationBaseClass.SimBaseClass()
-    dynProcess = scSim.CreateNewProcess(simProcessName)
-    simulationTimeStep = macros.sec2nano(1.0)
-    dynProcess.addTask(scSim.CreateNewTask(simTaskName, simulationTimeStep))
-
-    # initialize spacecraft object and set properties
+def setup_spacecraft(scSim):
     scObject = spacecraft.Spacecraft()
     scObject.ModelTag = "bsk-Sat"
-
-    # Define the simulation inertia
-    I = [900.0, 0.0, 0.0, 0.0, 800.0, 0.0, 0.0, 0.0, 600.0]
     scObject.hub.mHub = 750.0  # kg - spacecraft mass
-    scObject.hub.r_BcB_B = [
-        [0.0],
-        [0.0],
-        [0.0],
-    ]  # m - position vector of body-fixed point B relative to CM
-    scObject.hub.IHubPntBc_B = unitTestSupport.np2EigenMatrix3d(I)
-
-    # Add spacecraft object to the simulation process
+    scObject.hub.r_BcB_B = [[0.0], [0.0], [0.0]]
+    scObject.hub.IHubPntBc_B = unitTestSupport.np2EigenMatrix3d(intertia)
     scSim.AddModelToTask(simTaskName, scObject)
+    return scObject
 
+
+def setup_orbit(mu):
+    oe = orbitalMotion.ClassicElements()
+    oe.a = (6378 + 600) * 1000.0
+    oe.e = 0.01
+    oe.i = 63.3 * macros.D2R
+    oe.Omega = 88.2 * macros.D2R
+    oe.omega = 347.8 * macros.D2R
+    oe.f = 135.3 * macros.D2R
+    rN, vN = orbitalMotion.elem2rv(mu, oe)
+    return rN, vN, np.sqrt(mu / oe.a / oe.a / oe.a)
+
+
+def setup_thermal_sensor(spiceObject, scObject, scSim):
+    # Now add the thermal sensor module
+    thermalSensor = sensorThermal.SensorThermal()
+    thermalSensor.T_0 = 0  # Celsius
+    thermalSensor.nHat_B = [0, 0, 1]
+    thermalSensor.sensorArea = 1.0  # m^2
+    thermalSensor.sensorAbsorptivity = 0.25
+    thermalSensor.sensorEmissivity = 0.34
+    thermalSensor.sensorMass = 2.0  # kg
+    thermalSensor.sensorSpecificHeat = 890
+    thermalSensor.sensorPowerDraw = 30.0  # W
+    thermalSensor.sunInMsg.subscribeTo(spiceObject.planetStateOutMsgs[0])
+    thermalSensor.stateInMsg.subscribeTo(scObject.scStateOutMsg)
+    scSim.AddModelToTask(simTaskName, thermalSensor)
+    tempLog = thermalSensor.temperatureOutMsg.recorder()
+    scSim.AddModelToTask(simTaskName, tempLog)
+
+    return tempLog
+
+
+def setup_spice_interface(gravFactory, scSim):
+    spiceObject = gravFactory.createSpiceInterface(
+        time="2021 MAY 04 07:47:48.965 (UTC)"
+    )
+    scSim.AddModelToTask(simTaskName, spiceObject)
+
+    return spiceObject
+
+
+def setup_grav_body(scObject):
     # Create the gravFactory
     gravFactory = simIncludeGravBody.gravBodyFactory()
 
@@ -73,23 +87,12 @@ def run(show_plots):
     # Attach gravity model to spacecraft
     gravFactory.addBodiesTo(scObject)
 
-    # Create the spice interface
-    spiceObject = gravFactory.createSpiceInterface(
-        time="2021 MAY 04 07:47:48.965 (UTC)"
-    )
-    scSim.AddModelToTask(simTaskName, spiceObject)
+    return gravFactory, mu
 
-    # Set up the orbit using classical orbit elements
-    oe = orbitalMotion.ClassicElements()
-    oe.a = (6378 + 600) * 1000.0  # meters
-    oe.e = 0.01
-    oe.i = 63.3 * macros.D2R
-    oe.Omega = 88.2 * macros.D2R
-    oe.omega = 347.8 * macros.D2R
-    oe.f = 135.3 * macros.D2R
-    rN, vN = orbitalMotion.elem2rv(mu, oe)
-    n = np.sqrt(mu / oe.a / oe.a / oe.a)
+
+def setup_modules(scSim, scObject, spiceObject, n, rN, vN):
     P = 2.0 * np.pi / n
+
     scObject.hub.r_CN_NInit = rN  # m   - r_CN_N
     scObject.hub.v_CN_NInit = vN  # m/s - v_CN_N
     scObject.hub.sigma_BNInit = [[0.1], [0.2], [-0.3]]  # sigma_BN_B
@@ -135,33 +138,20 @@ def run(show_plots):
 
     # Connect torque command to external torque effector
     extFTObject.cmdTorqueInMsg.subscribeTo(mrpControl.cmdTorqueOutMsg)
+    return mrpControl, locPoint, P
 
-    # Now add the thermal sensor module
-    thermalSensor = sensorThermal.SensorThermal()
-    thermalSensor.T_0 = 0  # Celsius
-    thermalSensor.nHat_B = [0, 0, 1]
-    thermalSensor.sensorArea = 1.0  # m^2
-    thermalSensor.sensorAbsorptivity = 0.25
-    thermalSensor.sensorEmissivity = 0.34
-    thermalSensor.sensorMass = 2.0  # kg
-    thermalSensor.sensorSpecificHeat = 890
-    thermalSensor.sensorPowerDraw = 30.0  # W
-    thermalSensor.sunInMsg.subscribeTo(spiceObject.planetStateOutMsgs[0])
-    thermalSensor.stateInMsg.subscribeTo(scObject.scStateOutMsg)
-    scSim.AddModelToTask(simTaskName, thermalSensor)
 
-    # Setup logging on the power system
-    tempLog = thermalSensor.temperatureOutMsg.recorder()
-    scSim.AddModelToTask(simTaskName, tempLog)
+def setup_simulation():
+    scSim = SimulationBaseClass.SimBaseClass()
+    simulationTimeStep = macros.sec2nano(1.0)
 
-    # Create the FSW vehicle configuration message
-    vehicleConfigOut = messaging.VehicleConfigMsgPayload()
-    vehicleConfigOut.ISCPntB_B = (
-        I  # use the same inertia in the FSW algorithm as in the simulation
-    )
-    configDataMsg = messaging.VehicleConfigMsg().write(vehicleConfigOut)
-    mrpControl.vehConfigInMsg.subscribeTo(configDataMsg)
+    dynProcess = scSim.CreateNewProcess(simProcessName)
+    dynProcess.addTask(scSim.CreateNewTask(simTaskName, simulationTimeStep))
 
+    return scSim, simTaskName
+
+
+def start_simulation(scSim, locPoint, P):
     # Initialize the simulation
     scSim.InitializeSimulation()
 
@@ -179,33 +169,48 @@ def run(show_plots):
     scSim.ConfigureStopTime(macros.sec2nano(int(2 * P)))  # seconds to stop simulation
     scSim.ExecuteSimulation()
 
-    # Pull the temperature data
+
+def plot_results(tempLog):
     tempData = tempLog.temperature
+    tvec = tempLog.times() * macros.NANO2HOUR
 
-    tvec = tempLog.times()
-    tvec = tvec * macros.NANO2HOUR
-
-    #   Plot the power states
     figureList = {}
-    plt.close("all")  # clears out plots from earlier test runs
+    plt.close("all")
     plt.figure(1)
     plt.plot(tvec * 60.0, tempData)
     plt.xlabel("Time (min)")
     plt.ylabel("Temperature (deg C)")
     plt.grid(True)
+    figureList["scenario_thermalSensor"] = plt.figure(1)
 
-    pltName = "scenario_thermalSensor"
-    figureList[pltName] = plt.figure(1)
-
-    if show_plots:
-        plt.show()
+    plt.show()
     plt.close("all")
 
-    return figureList
+
+def run():
+    scSim, simTaskName = setup_simulation()
+
+    scObject = setup_spacecraft(scSim)
+
+    gravFactory, mu = setup_grav_body(scObject)
+
+    spiceObject = setup_spice_interface(gravFactory, scSim)
+
+    rN, vN, n = setup_orbit(mu)
+
+    mrpControl, locPoint, P = setup_modules(scSim, scObject, spiceObject, n, rN, vN)
+    tempLog = setup_thermal_sensor(spiceObject, scObject, scSim)
+
+    # Create the FSW vehicle configuration message
+    vehicleConfigOut = messaging.VehicleConfigMsgPayload()
+    vehicleConfigOut.ISCPntB_B = intertia
+    configDataMsg = messaging.VehicleConfigMsg().write(vehicleConfigOut)
+    mrpControl.vehConfigInMsg.subscribeTo(configDataMsg)
+
+    start_simulation(scSim, locPoint, P)
+
+    plot_results(tempLog)
 
 
-# This statement below ensures that the unitTestScript can be run as a
-# stand-alone python script
-#
 if __name__ == "__main__":
-    run(True)  # show_plots
+    run()
